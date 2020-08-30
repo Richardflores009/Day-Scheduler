f95335c53d35429543c3ceafd5cb798b0d22412a
Richy Rich <richardflores009@gmail.com>
2020-08-29 18:04:15 -0700

    added attributes

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,5 +62,13 @@ $(document).ready(function () {
 
 });
 
-
+$(".nine").val(localStorage.getItem("9AM"))
+$(".ten").val(localStorage.getItem("10AM"))
+$(".eleven").val(localStorage.getItem("11AM"))
+$(".twelve").val(localStorage.getItem("12PM"))
+$(".one").val(localStorage.getItem("1PM"))
+$(".two").val(localStorage.getItem("2PM"))
+$(".three").val(localStorage.getItem("3PM"))
+$(".four").val(localStorage.getItem("4PM"))
+$(".five").val(localStorage.getItem("5PM"))
 
