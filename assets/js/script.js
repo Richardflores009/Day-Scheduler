9c6046c5b85123780d98f172700b6d0c9b13386b
Richy Rich <richardflores009@gmail.com>
2020-08-29 17:56:54 -0700

    added time elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
-var tasks = {}
-var tasks = JSON.parse(localStorage.getItem("tasks")) || {};
+
 var currentDate = document.getElementById("currentDay")
 
 var currentDay = moment().format('dddd, MMMM Do YYYY')
-var currentHour = moment().format("h")
+var currentHour = moment().hours()
 $("#currentDay").replaceWith(currentDay)
 
-
+function updateColors() {
+    $(".time-color").each(function () {
+        var thisBlock = Number($(this).attr("data-name"))
+        if (thisBlock === currentHour) {
+            $(this).css("background-color", "salmon")
+        } else if (thisBlock < currentHour) {
+            console.log("less than")
+            $(this).css("background-color", "lightgray")
+        } else if (thisBlock > currentHour) {
+            console.log("greater than")
+            $(this).css("background-color", "lightgreen")
+        }
+    })
+}
+updateColors();
 // check if you can see textarea input
 // store input to local storage
 //
@@ -15,25 +28,22 @@ var taskEffect = function (data) {
     if (data == currentHour) {
         $(this).css("background-color", "red");
     }
-
-
-
     // if (moment().format("h").isAfter(data)) {
     //     $('textarea').addClass("list-group-item-warning");
     //     console.log(data)
     // }
-
 }
 taskEffect()
 
 
 var loadSchedule = function (value, hour) {
+    localStorage.setItem(hour, value);
 
-    tasks[hour] = value
+    // tasks[hour] = value
 
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-    // var content = $("div").children('div').text()
-    console.log(tasks.length)
+    // localStorage.setItem("tasks", JSON.stringify(tasks))
+    // // var content = $("div").children('div').text()
+    // console.log(tasks.length)
 
     // button name =
 }
@@ -52,4 +62,13 @@ $(document).ready(function () {
 
 });
 
+$(".nine").val(localStorage.getItem("9AM"))
+$(".ten").val(localStorage.getItem("10AM"))
+$(".eleven").val(localStorage.getItem("11AM"))
+$(".twelve").val(localStorage.getItem("12PM"))
+$(".one").val(localStorage.getItem("1PM"))
+$(".two").val(localStorage.getItem("2PM"))
+$(".three").val(localStorage.getItem("3PM"))
+$(".four").val(localStorage.getItem("4PM"))
+$(".five").val(localStorage.getItem("5PM"))
 
